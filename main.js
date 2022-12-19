e72d34d604ced2353e7d822b23b9bff71b716284
jiang <jiang@jiangdeMacBook-Pro.local>
2022-12-19 16:22:34 +0800

    完成了cate分类页面的开发

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ import { $http } from '@escook/request-miniprogram'
 
 uni.$http = $http
 // 配置请求根路径
-$http.baseUrl = 'https://www.uinav.com'
+$http.baseUrl = 'https://api-hmugo-web.itheima.net'
 
 // 请求开始之前做一些事情
 $http.beforeRequest = function (options) {
